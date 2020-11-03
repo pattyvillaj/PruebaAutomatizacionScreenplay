358ca5d56d052ee18f52eaf18ba17d81ba96feb1
Patricia Villamizar <Patty@Olivers-MacBook-Air.local>
2020-11-03 11:56:40 -0500

    Creación de proyecto

---
+++ src/main/java/co/com/choucair/certification/proyectobase/tasks/OpenUp.java
@@ -0,0 +1,24 @@
+package co.com.choucair.certification.proyectobase.tasks;
+
+import co.com.choucair.certification.proyectobase.userinterface.ChoucairAcademyPage;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Open;
+import org.aspectj.apache.bcel.classfile.Module;
+
+public class OpenUp implements Task {
+    private ChoucairAcademyPage choucairAcademyPage;
+
+
+    public static OpenUp thePage() {
+        return Tasks.instrumented(OpenUp.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(Open.browserOn(choucairAcademyPage));
+
+    }
+}
---
+++ src/test/resources/features/academyChoucair.feature
@@ -0,0 +1,24 @@
+# Actor: Brandon
+
+@stories
+  Feature: Academy Choucair
+    As a user, I want to learn how to automate in screenplay at the Choucair Academy with the automation course
+  @scenario1
+  Scenario: Search for a automation course
+    Given than brandon wants to learns automation at the Academy Choucair
+    | strUser      | strPassword    |
+    | 1090375676   | Choucair2020*  |
+
+
+    When he search for the course on the Choucair academy platform
+    | strCourse              |
+    | Curso Basico de Agile  |
+
+
+    Then he finds the course called
+    | strCourse              |
+    | Curso Basico de Agile  |
+
+
+
+
